1b87b9459a3ee7b24baf790965458cf9a24c6f7f
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-30 13:39:38 -0800

    Final commit! Figured out the last of the pesky queries!

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 	has_many :messages
 	has_many :owners
 	has_many :blogs, :through => :owners
+	has_many :post, :through => :blogs
 
 	EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
 	validates :first_name, :last_name, presence:true, length: { minimum: 2 }
