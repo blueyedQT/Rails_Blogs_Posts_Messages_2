4c8323e7b7e6675e35cacfece9090036c55fd799
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-30 12:49:06 -0800

    Saving current progress

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,8 @@
 class Blog < ActiveRecord::Base
 	has_many :posts
 	has_many :owners
-	has_many :users, through :owners
+	has_many :users, :through => :owners
+
+	validates :name, presence: true, length: { minimum: 3 }
+	validates :description, presence: true, length: { minimum: 15 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ActiveRecord::Base
 	has_many :posts
 	has_many :messages
 	has_many :owners
-	has_many :blogs, through :owners 
+	has_many :blogs, :through => :owners
+
+	EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+	validates :first_name, :last_name, presence:true, length: { minimum: 2 }
+	validates :email_address, presence:true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+	before_save do 
+		self.email.downcase!
+	end
 end
